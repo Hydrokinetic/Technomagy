18ae08a9a37dca404672c26b7eac4c7d537cc83c
Hydrokinetic <sonicx00222@gmail.com>
2017-10-19 17:50:05 -0400

    first blocks

---
+++ src/main/java/hydrokinetic/technomagy/ModBlocks.java
@@ -0,0 +1,24 @@
+/*
+ * This file was created by Hydrokinetic (aka Swordstone)
+ * as part of Technomagy, a mod for Minecraft. The source code
+ * can be found here:
+ * https://github.com/Hydrokinetic/Technomagy
+ * 
+ * Technomagy is open-source and distributed under the Creative
+ * Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ * license. 
+ * 
+ * If you're wondering what that means, a summary can be found here:
+ * https://creativecommons.org/licenses/by-nc-sa/3.0/
+ * 
+ * File creation date: Oct 19, 2017, 12:46:14 PM EST
+ */
+
+package hydrokinetic.technomagy;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModBlocks {
+    @GameRegistry.ObjectHolder("technomagy.firstBlock")
+    public static BlockFirst firstBlock;
+}
