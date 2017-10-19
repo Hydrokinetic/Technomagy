7e83a85bfe98579d1faf33429e1fc0d61cb6deff
Hydrokinetic <sonicx00222@gmail.com>
2017-10-19 12:09:33 -0400

    license headers and file structure

---
+++ src/main/java/hydrokinetic/technomagy/Technomagy.java
@@ -0,0 +1,31 @@
+/*
+ * This file was created by Hydrokinetic (aka Swordstone)
+ * as part of Technomagy, a mod for Minecraft. The source code
+ * can be found here:
+ * https://github.com/Hydrokinetic/Technomagy
+ * 
+ * Technomagy is open-source and distributed under the Creative
+ * Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ * license. 
+ * 
+ * If you're wondering what that means, a summary can be found here:
+ * https://creativecommons.org/licenses/by-nc-sa/3.0/
+ * 
+ * File creation date: Oct 19, 2017, 12:03:44 PM EST
+ */
+
+package hydrokinetic.technomagy;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Technomagy.MODID, name = Technomagy.MODNAME, version = Technomagy.MODVERSION, dependencies = "required-after:Forge@[14.23.0.2512,)", useMetadata = true)
+public class Technomagy {
+    
+    public static final String MODID = "technomagy";
+    public static final String MODNAME = "Technomagy";
+    public static final String MODVERSION = "0.0.1";
+
+}
