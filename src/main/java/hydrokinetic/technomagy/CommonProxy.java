3814cf5109ab1fb62e505d339182e34b7b654788
Hydrokinetic <sonicx00222@gmail.com>
2017-10-19 12:36:03 -0400

    added configs

---
+++ src/main/java/hydrokinetic/technomagy/CommonProxy.java
@@ -0,0 +1,58 @@
+/*
+ * This file was created by Hydrokinetic (aka Swordstone)
+ * as part of Technomagy, a mod for Minecraft. The source code
+ * can be found here:
+ * https://github.com/Hydrokinetic/Technomagy
+ * 
+ * Technomagy is open-source and distributed under the Creative
+ * Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+ * license. 
+ * 
+ * If you're wondering what that means, a summary can be found here:
+ * https://creativecommons.org/licenses/by-nc-sa/3.0/
+ * 
+ * File creation date: Oct 19, 2017, 12:16:04 PM EST
+ */
+
+package hydrokinetic.technomagy;
+
+import java.io.File;
+import net.minecraft.item.Item;
+import net.minecraft.block.Block;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+
+@Mod.EventBusSubscriber
+public class CommonProxy {
+    
+    public static Configuration config;
+    
+    public void preInit(FMLPreInitializationEvent e) {
+        File directory = e.getModConfigurationDirectory();
+        config = new Configuration(new File(directory.getPath(), "technomagy.cfg"));
+        Config.readConfig();
+    }
+
+    public void init(FMLInitializationEvent e) {
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+        if (config.hasChanged()) {
+            config.save();
+        }
+    }
+    
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {     
+    }
+    
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) {        
+    }
+}
